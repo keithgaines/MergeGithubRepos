ce068006e61e6f3f1fee0aaae982693bbb3c1d47
keithgaines <93451842+keithgaines@users.noreply.github.com>
2022-03-16 08:53:20 -0500

    Add files via upload

---
+++ Merge Github Repos.ps1
@@ -0,0 +1,20 @@
+﻿# cloned url is the url of the repo being cloned 
+# rmurl (receiving murge url) is the url of the repo being merged into
+
+$clonedurl = Read-Host "Please enter the URL of the repo to be cloned"
+$clonedreponame = Read-Host "Please enter the name of the repo being cloned"
+
+$rmurl = Read-Host "Please enter the URL of the repo being merged into"
+
+git clone $url
+cd $reponame
+git remote rm origin
+git remote add origin $rmurl
+git checkout main
+git branch -m main-holder
+git fetch
+git checkout main
+git pull origin main
+git merge main-holder --allow-unrelated-histories
+git push origin main
+cd ..
